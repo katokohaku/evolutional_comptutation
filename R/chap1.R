df2f11081d128c0d25dfd6b5f15cb224c1504449
KATO Kohaku <kato.satoshi.0@gmail.com>
2018-07-18 00:45:04 +0900

    add chap2: alternate()

--- R/chap1.R
+++ R/chap1.R
@@ -1,9 +1,10 @@
 require(dplyr)
+require(magrittr)
 require(foreach)
 
-GEN_MAX     = 1000 # number of chromration
+GEN_MAX     = 1000 # number of generation
 POP_SIZE    = 1000 # population size
-ELITE       = 1    # number of elite individual for next chromration
+ELITE       = 10   # number of elite individual for next chromration
 MUTATE_PROB = 0.01 # mutation rate
 N           = 64   # size of problem (= number of element)
 
@@ -13,7 +14,7 @@ N           = 64   # size of problem (= number of element)
 individual <- function(N){
   stopifnot(N > 1)
   
-  chrom <- sample(c(-1,1), N, replace=TRUE)
+  chrom <- sample(c(0,1), N, replace=TRUE)
   return(chrom)  
 }
 # example
@@ -21,9 +22,9 @@ individual <- function(N){
 
 
 fitness <- function(chrom){
-  stopifnot(all(chrom %in% c(-1,1)) == TRUE)
+  stopifnot(all(chrom %in% c(0,1)) == TRUE)
   return(
-    sum(chrom * seq_along(chrom)^0.5) %>% abs
+    sum((chrom *2 -1) * seq_along(chrom)^0.5) %>% abs
   )
 }
 # example
@@ -31,64 +32,139 @@ fitness <- function(chrom){
 fitness(ind1)
 
 
-crossover <- function(p1, p2){
+crossover <- function(p1, p2, show.pos = FALSE){
   len_chrom <- length(p1)
   stopifnot(len_chrom == length(p2))
   
   # position at crossover
-  at <- sample(len_chrom, 1)
+  at <- sample(len_chrom - 1 , 1)
   
-  child <- list(at = at,
-                chrom = c(p1[1:at], p2[(at+1):len_chrom]))
+  child <- c(p1[1:at], p2[(at+1):len_chrom])
+  if(show.pos){
+    child <- list(at = at, p1 = p1, p2 = p2, chrom = child)
+  }
   return(child)
 }
 # example
 (chr1 <- rep(1, 10))
 (chr2 <- rep(0, 10))
-crossover(chr1, chr2)
+crossover(chr1, chr2, show.pos = TRUE)
+crossover(chr1, chr2, show.pos = FALSE)
 
 
 mutation <- function(chrom, mutate.prob){
-  stopifnot(all(chrom %in% c(-1,1)) == TRUE,
+  stopifnot(all(chrom %in% c(0,1)) == TRUE,
             !missing(mutate.prob))
   
   # position at mutation
   at <- which(runif(length(chrom)) < mutate.prob)
-  
-  chrom[at] <- -chrom[at]
+  chrom[at] <- 1 - chrom[at]
   return(chrom)
 }
-
+# example
 c3 <- rep(1, 100)
 mutation(c3, 0.05)
 
 # population --------------------------------------------------------------
 
-(population <- foreach(i = 1:10) %do% individual(20))
-
-fits <- sapply(population, fitness)
-
-population2 <- foreach(i = order(fits)) %do% population[[i]]
-
-sort(fits)
-lapply(population2, fitness) %>% unlist
-
-# 順位に基づくランキング選択で親個体を2つ選択する
-# 戻り値: 選択した親個体の添え字
-selectParent <- function(population){
-  x <- seq_along(population)
-  y <- rev(x)
+initPopulation <- function(pop.size, chrom.size){
+  stopifnot(!missing(pop.size), !missing(chrom.size))
   
-  denom <- foreach(i = x, .combine = c) %do% rep(x[i], y[i])
-  return(sample(denom, 1))
+  population <- tibble(
+    chrom = foreach(i = 1:pop.size) %do% individual(chrom.size))
+  
+  population$fits <- sapply(population$chrom, fitness)
+  
+  return(population %>% arrange(fits))
 }
 
-p1 <- selectParent(population)
-p2 <- selectParent(population)
-while(p1 == p2){
-  p2 <- sample(denom, 1)
-  print(p2)  
-}
-c(p1, p2)
+# 
+# GEN_MAX     = 100  # number of generation
+# POP_SIZE    = 100  # population size
+# ELITE       = 10   # number of elite individual for next chromration
+# MUTATE_PROB = 0.01 # mutation rate
+# N           = 64   # size of problem (= number of element)
+# 
+# population <- initPopulation(pop.size = POP_SIZE, chrom.size = N)
+# population
+# 
+# pop.size = POP_SIZE
+# # Preserve elite individuals
+# nextInd.pres <- population %>% head(ELITE)
+# 
+# # Generate children via: selection -> crossover -> mutation
+# # roulette source based on rank for selection
+# x <- 1:NROW(population)
+# denom <- foreach(i = x, .combine = c) %do% rep(x[i], rev(x)[i])
+# 
+# # selection -> crossover
+# nextChrom <- foreach(i = (ELITE+1):pop.size) %do% {
+#   p1 <- sample(denom, 1)
+#   p2 <- sample(denom, 1)
+#   while(p1 == p2){
+#     p2 <- sample(denom, 1)
+#     # print(p2)  
+#   }
+#   crossover(population$chrom[p1] %>% unlist,
+#             population$chrom[p2] %>% unlist)
+# }
+# 
+# # mutation
+# nextInd.gen <- tibble(
+#   chrom = lapply(nextChrom, mutation, mutate.prob = MUTATE_PROB))
+# nextInd.gen$fits <- sapply(nextInd.gen$chrom, fitness)
+# 
+# population <- bind_rows(nextInd.pres, nextInd.gen) %>% 
+#   arrange(fits) 
+# 
+# population %>% head()
 
-sum(1:5)
+# popuration function -----------------------------------------------------
+
+alternate <- function(population, elite.size, mutate.prob = 0.01){
+  stopifnot(!missing(population), NROW(population) > elite.size )
+  
+  # Preserve elite individuals
+  nextInd.pres <- population %>%
+    arrange(fits) %>% 
+    head(elite.size)
+  # nextInd.pres %>% head %>% print()
+  
+  # Generate children via: selection -> crossover -> mutation
+  # roulette source based on rank for selection
+  x <- 1:NROW(population)
+  denom <- foreach(i = x, .combine = c) %do% rep(x[i], rev(x)[i])
+  
+  # selection -> crossover
+  nextChrom <- foreach(i = (elite.size+1):NROW(population)) %do% {
+    p1 <- sample(denom, 1)
+    p2 <- sample(denom, 1)
+    while(p1 == p2){
+      p2 <- sample(denom, 1)
+      # print(p2)  
+    }
+    crossover(population$chrom[p1] %>% unlist,
+              population$chrom[p2] %>% unlist)
+  }
+  
+  # mutation
+  nextInd.gen <- tibble(
+    chrom = lapply(nextChrom, mutation, mutate.prob = mutate.prob))
+  nextInd.gen$fits <- sapply(nextInd.gen$chrom, fitness)
+  
+  return( rbind(nextInd.pres, nextInd.gen) )
+}
+
+(pop <- initPopulation(pop.size = 150, chrom.size = 500))
+topTeam <- pop %>% head(1)
+
+for(i in 2:50){
+  nextpop <- alternate(pop, elite.size = 10, mutate.prob = 0.05)
+  topTeam[i, ] <- pop %>% head(1)
+
+  pop <- nextpop
+}
+
+plot(x=1:length(topTeam$fits), y=topTeam$fits,
+     type="b", xlab = "generation", ylab = "fitness")
+
